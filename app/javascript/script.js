6f93d89f30774a263fb271d05f05c7c3924e2cb8
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2026-04-08 15:26:47 +0100

    added hyperlink functionality to profile and characters buttons

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,8 +1,13 @@
 
 $(document).ready(function (){
-    console.log("Turbo loaded!");
+    // console.log("Turbo loaded!");
     var loginBtn = $("#profile-btn");
     loginBtn.click(function () {
         Turbo.visit("/profile");
+    });
+
+    var charactersBtn = $("#characters-btn");
+    charactersBtn.click(function () {
+        Turbo.visit("/character_creation");
     });
 });
