c3fa6eac4c7600abda167a9eae8adb2d030598c0
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2026-04-08 15:59:44 +0100

    updated anchor tags to ruby links. Also, added list of avatar icons.

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,13 +1,30 @@
 
 $(document).ready(function (){
     // console.log("Turbo loaded!");
-    var loginBtn = $("#profile-btn");
+    const avatarIcons = [
+        "https://images.unsplash.com/photo-1633506079263-7029b4f46762?auto=format&fit=crop&q=80&w=200&h=200",
+        "https://images.unsplash.com/photo-1657851613794-13616dbcc247?auto=format&fit=crop&q=80&w=200&h=200",
+        "https://images.unsplash.com/photo-1743247299142-35028faf885d?auto=format&fit=crop&q=80&w=200&h=200",
+        "https://images.unsplash.com/photo-1653368653487-f55b96d90853?auto=format&fit=crop&q=80&w=200&h=200",
+    ]
+
+    let loginBtn = $("#profile-btn");
     loginBtn.click(function () {
         Turbo.visit("/profile");
     });
 
-    var charactersBtn = $("#characters-btn");
+    let charactersBtn = $("#characters-btn");
     charactersBtn.click(function () {
         Turbo.visit("/character_creation");
+    });
+
+    let prevAvatarBtn = $("#prev-avatar-btn");
+    prevAvatarBtn.click(function () {
+        // Handle previous avatar logic
+    });
+
+    let nextAvatarBtn = $("#next-avatar-btn");
+    nextAvatarBtn.click(function () {
+        // Handle next avatar logic
     });
 });
